92bc6e1b22a67b9667e2de03e84a679de666a013
zivserphos <zivfromisrael@gmail.com>
2021-10-13 17:08:01 +0300

    04-chaining-promises

--- 04-chaining-promises/answers.js
+++ 04-chaining-promises/answers.js
@@ -7,9 +7,8 @@
  */
 function flatMapPromise(promise, asyncTransformer){
   return new Promise((resolve, reject) => {
-    promise
-      .then(/* IMPLEMENT ME! */);
-  });
+    promise.then((value) => resolve(asyncTransformer(value)))
+    .catch((err) => reject(err))});
 }
 
 /**
@@ -20,7 +19,7 @@ function flatMapPromise(promise, asyncTransformer){
  * @param {function} slowAsyncProcess 
  */
 function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess){
-  return firstPromise.then(/* IMPLEMENT ME! */);
+  return firstPromise.then((result) => slowAsyncProcess(result));
 }
 
 /**
